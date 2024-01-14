650b294c704985cd7f354db578f111e0dc88c753
Mark Watson <mwatson@prosaris.ca>
2024-01-14 17:42:11 -0400

    add basic chat ai

--- app/dashboard/ai/page.tsx
+++ app/dashboard/ai/page.tsx
@@ -1,27 +1,59 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import AnimatedText from '@/components/AnimatedText';
 
 // todo: replace with real AI response
-import { text } from './DemoScript';
+import AiPrompt from '@/components/AiPrompt';
 
 const Ai = () => {
+  const [answer, setAnswer] = useState('');
+
+  useEffect(() => {
+    const handleStorageUpdate = () => {
+      const answer = sessionStorage.getItem('answer');
+      if (answer) {
+        setAnswer(answer);
+      }
+    };
+
+    window.addEventListener('storageUpdate', handleStorageUpdate);
+
+    return () => {
+      window.removeEventListener('storageUpdate', handleStorageUpdate);
+    };
+  }, []);
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        textAlign: 'left',
-        border: '1px solid white',
-        margin: 4,
-        padding: 50,
-      }}
-    >
-      <AnimatedText
-        text={text}
-        show={true}
-        animated={true}
-        animationDelay={250}
-      />
-    </div>
+    <>
+      <div
+        style={{
+          display: 'flex',
+          textAlign: 'left',
+          border: '1px solid white',
+          margin: 4,
+          padding: 50,
+        }}
+      >
+        <AnimatedText
+          text={answer || 'Nothing to show yet...'}
+          show={true}
+          animated={true}
+          animationDelay={250}
+        />
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          textAlign: 'center',
+          margin: 8,
+        }}
+      >
+        <AiPrompt />
+      </div>
+    </>
   );
 };
 
