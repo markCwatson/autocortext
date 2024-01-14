aba96905809a778aea038cef9c574a38b8daa703
Mark Watson <mwatson@prosaris.ca>
2024-01-14 19:01:23 -0400

    added microsoft power bi embedding

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,12 @@
-import DialogModal from '@/components/DialogModal';
-import SignInButton from '@/components/SignInButton';
-import { getCurrentUser } from '@/lib/session';
-
 export default async function MainDashboard() {
-  return <div className="text-center">This will be the dashboard</div>;
+  return (
+    <div className="text-center">
+      <iframe
+        title="Power BI Report"
+        src={process.env.EMBED_URL}
+        allowFullScreen={true}
+        style={{ width: '100%', height: '90vh' }}
+      />
+    </div>
+  );
 }
