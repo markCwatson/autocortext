e3b2a8320da40890b0eeec1de95a3eebdf1fae5c
Mark Watson <mwatson@prosaris.ca>
2024-02-09 09:03:25 -0400

    maxDuration

--- app/api/read/route.ts
+++ app/api/read/route.ts
@@ -4,6 +4,10 @@ import { queryPineconeVectorStoreAndQueryLLM } from '@/lib/pinecone';
 
 const indexName = 'auto-cortext';
 
+// Vercel's max duration is up to 5 mins.
+// We are getting 15 second timeouts so increasing to 60 seconds.
+export const maxDuration = 60;
+
 export async function POST(req: NextRequest) {
   const body = await req.json();
 
